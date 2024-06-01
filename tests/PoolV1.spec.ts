ebca71d707e5f0d75f07e3588f6678ac84bb2a37
ipromise2324 <168allen55@gmail.com>
2024-06-01 23:55:57 +0800

    feat: add liqudity from pool

--- tests/PoolV1.spec.ts
+++ tests/PoolV1.spec.ts
@@ -4,6 +4,10 @@ import { PoolV1 } from '../wrappers/PoolV1';
 import { JettonWallet } from '../wrappers/JettonWallet';
 import '@ton/test-utils';
 import { compile } from '@ton/blueprint';
+import { loadJMBondFixture, buyToken } from './helper';
+import { DexRouter } from '../wrappers/DexRouter';
+import { JettonMasterBondV1 } from '../wrappers/JettonMasterBondV1';
+import { Op } from '../wrappers/JettonConstants';
 
 describe('PoolV1', () => {
     let blockchain: Blockchain;
@@ -11,6 +15,8 @@ describe('PoolV1', () => {
     let poolV1: SandboxContract<PoolV1>;
     let poolV1Code: Cell;
     let jettonWalletCode: Cell;
+    let dexRouter: SandboxContract<DexRouter>;
+    let jettonMasterBondV1: SandboxContract<JettonMasterBondV1>;
 
     beforeAll(async () => {
         poolV1Code = await compile(PoolV1.name);
@@ -19,36 +25,14 @@ describe('PoolV1', () => {
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
-
         deployer = await blockchain.treasury('deployer');
 
-        poolV1 = blockchain.openContract(
-            PoolV1.createFromConfig(
-                {
-                    adminAddress: deployer.address,
-                    dexRouter: deployer.address,
-                    asset0: deployer.address, // for test
-                    asset1: deployer.address, // for test
-                    reserve0: toNano('0'),
-                    reserve1: toNano('0'),
-                    lpTotalSupply: toNano('0'),
-                    adminFee: toNano('0'),
-                    swapFee: toNano('0'),
-                    lpJettonWalletCode: jettonWalletCode,
-                    lpJettonContent: jettonWalletCode, // for test
-                },
-                poolV1Code,
-            ),
-        );
-
-        const deployResult = await poolV1.sendDeploy(deployer.getSender(), toNano('0.05'));
-
-        expect(deployResult.transactions).toHaveTransaction({
-            from: deployer.address,
-            to: poolV1.address,
-            deploy: true,
-            success: true,
-        });
+        ({ blockchain, deployer, dexRouter, jettonMasterBondV1 } = await loadJMBondFixture());
+        const buyer = await blockchain.treasury('buyer', { workchain: 0, balance: toNano('10000000') });
+        const buyTon = toNano('1000000');
+        await buyToken(jettonMasterBondV1, buyer, buyTon);
+        let poolAddress = await dexRouter.getPoolAddress(jettonMasterBondV1.address);
+        poolV1 = blockchain.openContract(PoolV1.createFromAddress(poolAddress));
     });
 
     it('should deploy', async () => {
@@ -56,28 +40,4 @@ describe('PoolV1', () => {
         // blockchain and poolV1 are ready to use
     });
 
-    it('should first deposit', async () => {
-        const depositResult = await poolV1.sendDeposit(
-            deployer.getSender(),
-            { value: toNano('100') },
-            {
-                $$type: 'Deposit',
-                queryId: BigInt(0),
-                asset0Amount: toNano('100'),
-                asset1Amount: toNano('100'),
-                minLpAmount: toNano('0'),
-                lpReceiver: null,
-                fulfillPayload: null,
-                rejectPayload: null,
-            },
-        );
-
-        printTransactionFees(depositResult.transactions);
-
-        expect(depositResult.transactions).toHaveTransaction({
-            from: deployer.address,
-            to: poolV1.address,
-            success: true,
-        });
-    });
 });
