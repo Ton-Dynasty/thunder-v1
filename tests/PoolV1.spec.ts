d3e141fa904ce70ce9a3182924186de747108c65
grace0950 <pinyan0950@gmail.com>
2024-06-01 15:39:17 +0800

    Update error constants in pool and jetton wallet contracts

--- tests/PoolV1.spec.ts
+++ tests/PoolV1.spec.ts
@@ -1,26 +1,45 @@
-import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
-import { Cell, toNano } from '@ton/core';
+import { Blockchain, printTransactionFees, SandboxContract, TreasuryContract } from '@ton/sandbox';
+import { Address, Cell, toNano } from '@ton/core';
 import { PoolV1 } from '../wrappers/PoolV1';
+import { JettonWallet } from '../wrappers/JettonWallet';
 import '@ton/test-utils';
 import { compile } from '@ton/blueprint';
 
 describe('PoolV1', () => {
-    let code: Cell;
-
-    beforeAll(async () => {
-        code = await compile('PoolV1');
-    });
-
     let blockchain: Blockchain;
     let deployer: SandboxContract<TreasuryContract>;
     let poolV1: SandboxContract<PoolV1>;
+    let poolV1Code: Cell;
+    let jettonWalletCode: Cell;
+
+    beforeAll(async () => {
+        poolV1Code = await compile(PoolV1.name);
+        jettonWalletCode = await compile(JettonWallet.name);
+    });
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
 
-        poolV1 = blockchain.openContract(PoolV1.createFromConfig({}, code));
-
         deployer = await blockchain.treasury('deployer');
+
+        poolV1 = blockchain.openContract(
+            PoolV1.createFromConfig(
+                {
+                    adminAddress: deployer.address,
+                    dexRouter: deployer.address,
+                    asset0: deployer.address, // for test
+                    asset1: deployer.address, // for test
+                    reserve0: toNano('0'),
+                    reserve1: toNano('0'),
+                    lpTotalSupply: toNano('0'),
+                    adminFee: toNano('0'),
+                    swapFee: toNano('0'),
+                    lpJettonWalletCode: jettonWalletCode,
+                    lpJettonContent: jettonWalletCode, // for test
+                },
+                poolV1Code,
+            ),
+        );
 
         const deployResult = await poolV1.sendDeploy(deployer.getSender(), toNano('0.05'));
 
@@ -35,5 +54,30 @@ describe('PoolV1', () => {
     it('should deploy', async () => {
         // the check is done inside beforeEach
         // blockchain and poolV1 are ready to use
+    });
+
+    it('should first deposit', async () => {
+        const depositResult = await poolV1.sendDeposit(
+            deployer.getSender(),
+            { value: toNano('100') },
+            {
+                $$type: 'Deposit',
+                queryId: BigInt(0),
+                asset0Amount: toNano('100'),
+                asset1Amount: toNano('100'),
+                minLpAmount: toNano('0'),
+                lpReceiver: null,
+                fulfillPayload: null,
+                rejectPayload: null,
+            },
+        );
+
+        printTransactionFees(depositResult.transactions);
+
+        expect(depositResult.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: poolV1.address,
+            success: true,
+        });
     });
 });
--- wrappers/PoolV1.ts
+++ wrappers/PoolV1.ts
@@ -1,13 +1,84 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
+import {
+    Address,
+    beginCell,
+    Builder,
+    Cell,
+    Contract,
+    contractAddress,
+    ContractProvider,
+    Sender,
+    SendMode,
+} from '@ton/core';
+import { Maybe } from '@ton/core/dist/utils/maybe';
 
-export type PoolV1Config = {};
+export type PoolV1Config = {
+    adminAddress: Address;
+    dexRouter: Address;
+    asset0: Address;
+    asset1: Address;
+    reserve0: bigint;
+    reserve1: bigint;
+    lpTotalSupply: bigint;
+    adminFee: bigint;
+    swapFee: bigint;
+    lpJettonWalletCode: Cell;
+    lpJettonContent: Cell;
+};
 
 export function poolV1ConfigToCell(config: PoolV1Config): Cell {
-    return beginCell().endCell();
+    let initData = beginCell()
+        .storeAddress(config.dexRouter)
+        .storeAddress(config.asset0)
+        .storeAddress(config.asset1)
+        .endCell();
+
+    return beginCell()
+        .storeAddress(config.adminAddress)
+        .storeCoins(config.reserve0)
+        .storeCoins(config.reserve1)
+        .storeCoins(config.lpTotalSupply)
+        .storeCoins(config.adminFee)
+        .storeCoins(config.swapFee)
+        .storeRef(initData)
+        .storeRef(config.lpJettonWalletCode)
+        .storeRef(config.lpJettonContent)
+        .endCell();
+}
+
+const PoolOpcodes = {
+    TopUp: 0xd372158c,
+    Deposit: 0x95db9d39,
+};
+
+export type Deposit = {
+    $$type: 'Deposit';
+    queryId: bigint;
+    asset0Amount: bigint;
+    asset1Amount: bigint;
+    minLpAmount: bigint;
+    lpReceiver: Maybe<Address>;
+    fulfillPayload: Maybe<Cell>;
+    rejectPayload: Maybe<Cell>;
+};
+
+export function storeDeposit(src: Deposit) {
+    return (b: Builder) => {
+        b.storeUint(PoolOpcodes.Deposit, 32);
+        b.storeUint(src.queryId, 64);
+        b.storeCoins(src.asset0Amount);
+        b.storeCoins(src.asset1Amount);
+        b.storeCoins(src.minLpAmount);
+        b.storeAddress(src.lpReceiver);
+        b.storeMaybeRef(src.fulfillPayload);
+        b.storeMaybeRef(src.rejectPayload);
+    };
 }
 
 export class PoolV1 implements Contract {
-    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
+    constructor(
+        readonly address: Address,
+        readonly init?: { code: Cell; data: Cell },
+    ) {}
 
     static createFromAddress(address: Address) {
         return new PoolV1(address);
@@ -19,11 +90,32 @@ export class PoolV1 implements Contract {
         return new PoolV1(contractAddress(workchain, init), init);
     }
 
+    /* Pack */
+    static packDeposit(src: Deposit) {
+        return beginCell().store(storeDeposit(src)).endCell();
+    }
+
+    /* Send */
     async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
         await provider.internal(via, {
             value,
             sendMode: SendMode.PAY_GAS_SEPARATELY,
-            body: beginCell().endCell(),
+            body: beginCell().storeUint(PoolOpcodes.TopUp, 32).storeUint(0, 64).endCell(),
+        });
+    }
+
+    async sendDeposit(
+        provider: ContractProvider,
+        via: Sender,
+        args: { value: bigint; bounce?: boolean },
+        body: Deposit,
+        sendMode?: SendMode,
+    ) {
+        await provider.internal(via, {
+            value: args.value,
+            bounce: args.bounce,
+            sendMode: sendMode,
+            body: PoolV1.packDeposit(body),
         });
     }
 }
