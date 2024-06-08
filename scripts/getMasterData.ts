270dcaaaa3a5ed57906ba6063927f35855cd1d6b
ipromise2324 <168allen55@gmail.com>
2024-06-08 17:32:58 +0800

    feat: drease gas fee

--- scripts/getMasterData.ts
+++ scripts/getMasterData.ts
@@ -3,7 +3,7 @@ import { NetworkProvider } from '@ton/blueprint';
 import { JettonMasterBondV1 } from '../wrappers/JettonMasterBondV1';
 
 export async function run(provider: NetworkProvider) {
-    const jettonMasterAddress = Address.parse('kQAw3WvUAEr4Iqs0wy6X14oGjLMtv6MlUFa9UXTw4NDF5m0O');
+    const jettonMasterAddress = Address.parse('EQD0wJBMxf1FfQUT2GoANbiqeTVSE2_TzqoweI6o49uA5BOf');
     const jettonMasterBondV1 = provider.open(JettonMasterBondV1.createFromAddress(jettonMasterAddress));
     let masterData = await jettonMasterBondV1.getMasterData();
     console.log(masterData);
